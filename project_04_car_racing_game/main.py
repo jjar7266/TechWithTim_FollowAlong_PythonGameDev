9b5ebfffc5158f302c689562dc76db6e3c476e8f
jjar7266 <jjar7266@gmail.com>
2026-07-20 23:23:47 -0400

    Project 04 - Car Racing Game: Completed Part 2 by adding collision masks, finish line logic, bounce/reset behavior, and tuning car scale for proper game feel.

--- project_04_car_racing_game/main.py
+++ project_04_car_racing_game/main.py
@@ -24,6 +24,11 @@ TRACK         = pygame.image.load(IMGS_DIR / "track.png")
 TRACK_BORDER  = pygame.image.load(IMGS_DIR / "track-border.png")
 FINISH        = pygame.image.load(IMGS_DIR / "finish.png")
 
+# Added in part 2
+FINISH       = scale_image(FINISH, 0.8)
+FINISH_MASK     = pygame.mask.from_surface(FINISH)
+FINISH_POSITION = (118, 222)
+
 RED_CAR       = pygame.image.load(IMGS_DIR / "red-car.png")
 GREEN_CAR     = pygame.image.load(IMGS_DIR / "green-car.png")
 GREY_CAR      = pygame.image.load(IMGS_DIR / "grey-car.png")
@@ -35,12 +40,18 @@ WHITE_CAR     = pygame.image.load(IMGS_DIR / "white-car.png")
 # ------------------------------------------------------------
 TRACK        = scale_image(TRACK, 0.8)                # shrink track
 TRACK_BORDER = scale_image(TRACK_BORDER, 0.8)  # same size as track
+
+# Added in part 2
+TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)  # added in part 2
+
 GRASS        = scale_image(GRASS, 2.5)
-RED_CAR      = scale_image(RED_CAR, 0.50)
-GREEN_CAR    = scale_image(GREEN_CAR, 0.50)
-GREY_CAR     = scale_image(GREY_CAR, 0.50)
-PURPLE_CAR   = scale_image(PURPLE_CAR, 0.50)
-WHITE_CAR    = scale_image(WHITE_CAR, 0.50)
+RED_CAR      = scale_image(RED_CAR, 0.44)
+GREEN_CAR    = scale_image(GREEN_CAR, 0.44)
+GREY_CAR     = scale_image(GREY_CAR, 0.44)
+PURPLE_CAR   = scale_image(PURPLE_CAR, 0.44)
+WHITE_CAR    = scale_image(WHITE_CAR, 0.44)
+
+
 
 # Window matches scaled track
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
@@ -57,7 +68,7 @@ class AbstractCar:
         self.rotation_vel = rotation_vel
         self.angle = 0
         self.x, self.y = self.START_POS
-        self.acceleration = 0.1
+        self.acceleration = 0.088
 
     def rotate(self, left=False, right=False):
         if left:
@@ -73,6 +84,10 @@ class AbstractCar:
         self.vel = min(self.vel + self.acceleration, self.max_vel)
         self.move()
 
+    def move_backward(self):
+        self.vel = max(self.vel - self.acceleration, -self.max_vel / 2)
+        self.move()
+
     def move(self):
         radians = math.radians(self.angle)
         vertical = math.cos(radians) * self.vel
@@ -81,15 +96,32 @@ class AbstractCar:
         self.y -= vertical
         self.x -= horizontal
 
-    def reduce_speed(self):
-        self.vel = max(self.vel - self.acceleration / 2, 0)
-        self.move()
+    # added in part 2
+    def collide(self, mask, x=0, y=0):
+        car_mask = pygame.mask.from_surface(self.img)
+        offset = (int(self.x - x), int(self.y - y))
+        poi = mask.overlap(car_mask, offset)
+        return poi
+
+    # added in part 2
+    def reset(self):
+        self.x, self.y = self.START_POS
+        self.angle = 0
+        self.vel = 0
 
 
 
 class PlayerCar(AbstractCar):
     IMG = RED_CAR
     START_POS = (160, 178)  # not the same as instructors starting point
+
+    def reduce_speed(self):
+        self.vel = max(self.vel - self.acceleration / 2, 0)
+        self.move()
+
+    def bounce(self):
+        self.vel = -self.vel
+        self.move()
 
 
 # Draw Function
@@ -101,23 +133,8 @@ def draw(win, images, player_car):
     pygame.display.update()
 
 
-# ------------------------------------------------------------
-# GAME LOOP
-# ------------------------------------------------------------
-run = True
-clock = pygame.time.Clock()
-images = [(GRASS, (0, 0)), (TRACK, (0, 0))]
-player_car = PlayerCar(4, 4)
-
-while run:
-    clock.tick(FPS)
-
-    draw(WIN, images, player_car)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-            break
+# Move Player
+def move_player(player_car):
 
     keys = pygame.key.get_pressed()
     moved = False
@@ -129,16 +146,57 @@ while run:
     if keys[pygame.K_d]:
         player_car.rotate(right=True)
 
-    # Move Car
+    # Move Car forward
     if keys[pygame.K_w]:
         moved = True
         player_car.move_forward()
+
+    # Move Car backward
+    if keys[pygame.K_s]:
+        moved = True
+        player_car.move_backward()
 
     # Reduce speed
     if not moved:
         player_car.reduce_speed()
 
 
+# ------------------------------------------------------------
+# GAME LOOP
+# ------------------------------------------------------------
+run = True
+clock = pygame.time.Clock()
+images = [(GRASS, (0, 0)), (TRACK, (0, 0)),
+         (FINISH, FINISH_POSITION),
+         (TRACK_BORDER, (0, 0))]
+
+player_car = PlayerCar(1.5, 3)
+
+while run:
+    clock.tick(FPS)
+
+    draw(WIN, images, player_car)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            break
+
+    move_player(player_car)
+
+    # Added in part 2
+    if player_car.collide(TRACK_BORDER_MASK) != None:
+        player_car.bounce()
+
+    # Added in part 2
+    finish_poi_collide = player_car.collide(FINISH_MASK, *FINISH_POSITION)
+    if finish_poi_collide != None:
+        if finish_poi_collide[1] == 0:
+            player_car.bounce()
+
+        else:
+            player_car.reset()
+            print("FINISH")
 
 
 
