915f4dbd09023e60adcf673add42b822bbc89e1a
jjar7266 <jjar7266@gmail.com>
2026-07-23 19:52:37 -0400

    Project 05 - Checkers - initial commit: added project structure with main script, checkers package, board and piece classes, constants, and assets folder.. of Part 1

---
+++ project_05_checkers/checkers/constants.py
@@ -0,0 +1,20 @@
+# 14 Hours of Python Game Development - from Beginner to Advanced
+# Instructor: Tech With Tim
+# Followed along and coded by: Jose 'Joe' Ruiz
+# PYTHON CHECKERS TUTORIAL - PART 1 (Medium)
+# constants.py
+
+# THIS FILE WILL HOLD ALL THE CONSTANTS (specific to Checkers)
+
+# CONSTANTS
+WIDTH, HEIGHT = 800, 700
+ROWS, COLS    = 8, 8
+SQUARE_SIZE   = WIDTH // COLS
+
+# COLORS
+WHITE = (255, 255, 255)
+BLACK = (  0,   0,   0)
+BLUE  = (  0,   0, 255)
+GREY  = (128, 128, 128)
+RED   = (255,   0,   0)
+
